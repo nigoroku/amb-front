5f50b4344fbb3d7fbfec82c9f22edaed69ebad24
moizumi <mtcth210@gmail.com>
2020-12-28 01:38:16 +0900

    期限ぎれtokenの場合、404ページを表示するよう変更

--- pages/public.vue
+++ pages/public.vue
@@ -161,11 +161,11 @@ export default {
         if (userId != null && userId != "") {
           self.$store.commit("setPublicUserId", { publicUserId: userId });
         } else {
-          // self.$router.push("/notfound/");
+          self.$router.push("/notfound/");
         }
       })
       .catch(function () {
-        // self.$router.push("/notfound/");
+        self.$router.push("/notfound/");
       })
       .finally(function () {});
   },
